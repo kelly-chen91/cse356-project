e6adc3781e16f782761d1b4874cdfbccda8e7356
Kelly Chen <kellychen966@gmail.com>
2024-10-26 21:34:04 -0400

    test manifest

--- milestone1/src/routes/authRoutes.js
+++ milestone1/src/routes/authRoutes.js
@@ -262,6 +262,8 @@ router
   })
   .get("/api/manifest/:id", (res, req) => {
     const { id } = req.params.id;
+    const mediaPath = path.resolve("../milestone1/media");
+    res.sendFile(`${mediaPath}/${id}`);
   });
 
 module.exports = router;
