5d98a48feb9c62c3b1563aa2b28e421d6f229fd1
Zhenting Ling <zhenting.ling@stonybrook.edu>
2024-11-01 16:10:51 -0400

    Fix pad_video.sh

--- scripts/pad_video.sh
+++ scripts/pad_video.sh
@@ -7,8 +7,6 @@ for video in ../videos/*.mp4; do
     # Get the filename without the path and extension
     filename=$(basename "$video" .mp4)
 
-    mkdir -p "padded_videos"
-
     # Reprocess the video to 16:9 with black padding.
     ffmpeg -i "$video" \
     -vf "scale=w=iw*min(1280/iw\,720/ih):h=ih*min(1280/iw\,720/ih),pad=1280:720:(1280-iw*min(1280/iw\,720/ih))/2:(720-ih*min(1280/iw\,720/ih))/2" \
