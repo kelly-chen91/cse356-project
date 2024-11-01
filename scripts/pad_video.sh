93f2df9f2f93ecc172573fa0f6d7f01a6e999d89
Zhenting Ling <zhenting.ling@stonybrook.edu>
2024-11-01 13:56:08 -0400

    Change the pad_video script so it creates a directory in parent directory.

--- scripts/pad_video.sh
+++ scripts/pad_video.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Make sure your current directory is milestone1
 # Loop over all mp4 files in the "videos" folder
+
+mkdir -p "../padded_videos"
 for video in ../videos/*.mp4; do
     # Get the filename without the path and extension
     filename=$(basename "$video" .mp4)
@@ -11,7 +13,7 @@ for video in ../videos/*.mp4; do
     ffmpeg -i "$video" \
     -vf "scale=w=iw*min(1280/iw\,720/ih):h=ih*min(1280/iw\,720/ih),pad=1280:720:(1280-iw*min(1280/iw\,720/ih))/2:(720-ih*min(1280/iw\,720/ih))/2" \
     -c:a copy \
-    "padded_videos/${filename}.mp4" -y
+    "../padded_videos/${filename}.mp4" -y
 
     echo "Padded padded_videos/${filename}.mp4"
 done
