775aae4c322e9a2fe2c5bb9259fe552ec08f7a6d
Zhenting Ling <zhenting.ling@stonybrook.edu>
2024-11-13 15:11:45 +0000

    Delete Gorse from populateDB.js

--- scripts/populateDB.js
+++ scripts/populateDB.js
@@ -3,21 +3,10 @@ import fs from "fs";
 import Video from "../src/models/videos.js";
 import path from "path";
 import dotenv from "dotenv";
-
-
-const gorse = new Gorse({
-  endpoint: "http://127.0.0.1:8088",
-  secret: "zhenghaoz",
-  debug: true,
-});
-
-console.log("GORSE: ", gorse);
 dotenv.config();
 
 const jsonPath = path.resolve("../m2.json");
 const jsonData = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
-
-const items = [];
 
 mongoose
   .connect("mongodb://root:example@localhost:27017/warmup?authsource=admin", {
@@ -42,44 +31,11 @@ mongoose
         status: "complete",
       });
       await newVideo.save();
-
-      // Populate gorse items.
-      console.log(`Marking item ${newVideo._id} into gorse...`);
-      items.push({
-        ItemId: newVideo._id,
-        IsHidden: false,
-      });
+      console.log(`Saved _id: ${newVideo._id}`);
     }
 
     console.log("Data inserted successfully to mongoDB!");
     mongoose.disconnect();
-    // Inserting items to gorse.
-    console.log("Inserting items into gorse...");
-
-    gorse
-      .upsertItems(items)
-      .then(async (res) => {
-        console.log(`Successfully inserted item to Gorse! ${res}`);
-        await gorse.insertUser({ userId: "bob" });
-        await client.insertFeedbacks([
-          { FeedbackType: "like", UserId: "bob", ItemId: items[0].ItemId },
-        ]);
-
-        gorse
-          .getRecommend({
-            userId: "bob",
-            cursorOptions: { n: 10 },
-          })
-          .then((rec) => {
-            console.log("RECOMMENDATIONS: ", rec);
-          })
-          .catch((err) => {
-            console.log(`This is doomed: ${err}`);
-          });
-      })
-      .catch((err) => {
-        console.log(`Failed to insert to Gorse :( ${err}`);
-      });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
