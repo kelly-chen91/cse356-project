13a3bb8310f28e106c50560b600be8861ca5c027
Kelly Chen <kellychen966@gmail.com>
2024-11-23 00:13:13 -0500

    bleeeep

--- ffmpeg-queue/app.py
+++ ffmpeg-queue/app.py
@@ -93,6 +93,8 @@ def worker():
         for message in pubsub.listen():
             # task_data = redis_client.blpop("ffmpeg_tasks", timeout=10)
             logging.info(f"message: {message}")
+            task = json.loads(message['data'])
+            logging.info(f"Received task: {task}")
             # if message:
             #     _, task_json = message
             #     task = json.loads(task_json)
