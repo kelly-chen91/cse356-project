02371e7b507bd44ccb9705a47c9b5cbef439b9a0
Kelly Chen <kellychen966@gmail.com>
2024-11-22 22:39:57 -0500

    fix task

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -210,7 +210,7 @@ router
     res.status(200).json({ status: "OK", id: videoId });
 
     //Insert Redis queue here
-
+    const task = JSON.stringify({ videoName, videoId });
     taskQueue.rpush("ffmpeg_tasks", task, (err) => {
       if (err) {
         console.error("Error adding task to queue:", err);
