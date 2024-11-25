d6f1bafcf543bad5d5e270379a20c64ff8aa98d3
root <root@ms3.cloud.local>
2024-11-25 20:26:23 +0000

    /view route fixed.

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,43 @@
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 dotenv.config();
 import mongoose from "mongoose";
+
+const client = new MongoClient(process.env.MONGO_URL);
+
+const database = client.db("warmup");
+
+const replicas = 1; 
+
+// Get All Videos, Users
+const getAll = async (collection) => {
+  const col = database.collection(collection);
+  return await col.find().toArray();
+}
+
+/**
+ * 
+ * @param {String} collection 
+ * @param {JS Object} filter 
+ * @returns document
+ */
+const getOne = async (collection, filter) => {
+  const col = database.collection(collection);
+  return await col.findOne(filter);
+}
+
+// Insert User, Video
+const insertOne = async (collection, data) => {
+  const col = database.collection(collection);
+  return await col.insertOne(data, {writeConcern: {w:replicas, wtimeout: 10000}});
+}
+
+// Increment 
+// Update
+const updateOne = async (collection, filter, data) => {
+  const col = database.collection(collection);
+  return await col.updateOne(filter, data, {writeConcern: {w:replicas, wtimeout: 10000}})
+}
 
 const connectDB = async () => {
   try {
@@ -17,4 +54,4 @@ const connectDB = async () => {
   }
 };
 
-export { connectDB };
+export { connectDB, getAll, getOne, insertOne, updateOne };
